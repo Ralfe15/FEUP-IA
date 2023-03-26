af6148e8026c2744494c69a8c04aa8423798c981
Rafael Schmidt <up202000166@fe.up.pt>
2023-03-26 13:26:41 +0100

    wip

--- game.py
+++ game.py
@@ -3,7 +3,10 @@ import random
 
 BOARD_SIZE = 6
 DEBUG = False
-
+UP = 1
+DOWN = 2
+LEFT = 3
+RIGHT = 4
 
 def generate_initial_state():
     # Returns a game in initial state: a 6x6 board with 4 players in top left and bottom right corners
@@ -13,6 +16,12 @@ def generate_initial_state():
         BOARD_SIZE - 1] \
         = board[BOARD_SIZE - 2][BOARD_SIZE - 2] = Tile(2)
     return board
+
+def manhattan_distance(coords_piece, player):
+    if player == 2:
+        return coords_piece[0] + coords_piece[1]
+    elif player==1:
+        return (BOARD_SIZE-1 - coords_piece[0]) + (BOARD_SIZE-1 -coords_piece[1])
 
 
 class Game:
@@ -25,10 +34,11 @@ class Game:
 class Tile:
     def __init__(self, value=0):
         self.value = value
-        self.walls = [1 if random.randint(0,1) <= 0.3 else 0 for _ in range(4)]
+        self.walls = [1 if random.randint(
+            0, 1) <= 0.3 else 0 for _ in range(4)]
 
     def __str__(self):
-        
+
         return "{}".format(self.value)
 
     def has_wall_up(self):
@@ -43,10 +53,6 @@ class Tile:
     def has_wall_left(self):
         return self.walls[3]
 
-    #  || _ ¯¯¯¯¯¯
-    #  ¯
-
-    
 
 class GameState:
     # To access a (x, y) coord on the board: self.board[y][x] starting at (0,0)
@@ -57,9 +63,9 @@ class GameState:
         self.p2_pieces = []
         for i in range(len(self.board)):
             for j in range(len(self.board)):
-                if self.board[i][j] == 1:
+                if self.board[i][j].value == 1:
                     self.p1_pieces.append([i, j])
-                elif self.board[i][j] == 2:
+                elif self.board[i][j].value == 2:
                     self.p2_pieces.append([i, j])
 
     # Get available moves for piece at coords (x, y)
@@ -67,50 +73,108 @@ class GameState:
     # Legal moves are: moving to an empty space left/right/up/down, leaping one piece to an empty space
     def get_moves(self, x, y):
         moves = []
+        
+        available_cost = 3
         if x not in range(BOARD_SIZE) or y not in range(BOARD_SIZE) or self.board[x][y].value == 0:
             return []
 
         # Check up
         if y - 1 in range(BOARD_SIZE) and self.board[y - 1][x].value == 0:
-            if DEBUG: print("move up")
-            moves.append([(x, y - 1), 1])
+            moves.append([(x, y - 1), self.check_walls_simple_move(x,y, UP)])
+
         # Check down
         if y + 1 in range(BOARD_SIZE) and self.board[y + 1][x].value == 0:
-            if DEBUG: print("move down")
-            moves.append([(x, y + 1), 1])
+            moves.append([(x, y + 1), self.check_walls_simple_move(x,y, DOWN)])
+
         # Check right
         if x + 1 in range(BOARD_SIZE) and self.board[y][x + 1].value == 0:
-            if DEBUG: print("move right")
             moves.append([(x + 1, y), 1])
+
         # Check left
         if x - 1 in range(BOARD_SIZE) and self.board[y][x - 1].value == 0:
-            if DEBUG: print("move left")
             moves.append([(x - 1, y), 1])
 
+
         # Check leap up
-        if y - 2 in range(BOARD_SIZE) and self.board[y - 1][x].value != 0 and self.board[y - 2][x].value == 0:
-            if DEBUG: print("leap up")
+        if y - 2 in range(BOARD_SIZE) and self.board[y - 1][x].value != 0 and self.board[y - 2][x].value == 0 and self.check_can_leap(x, y, UP):
             moves.append([(x, y - 2), 1])
+
         # Check leap down
-        if y + 2 in range(BOARD_SIZE) and self.board[y + 1][x].value != 0 and self.board[y + 2][x].value == 0:
-            if DEBUG: print("leap down")
+        if y + 2 in range(BOARD_SIZE) and self.board[y + 1][x].value != 0 and self.board[y + 2][x].value == 0 and self.check_can_leap(x, y, DOWN):
             moves.append([(x, y + 2), 1])
+
         # Check leap right
-        if x + 2 in range(BOARD_SIZE) and self.board[y][x + 1].value != 0 and self.board[y][x + 2].value == 0:
-            if DEBUG: print("leap right")
+        if x + 2 in range(BOARD_SIZE) and self.board[y][x + 1].value != 0 and self.board[y][x + 2].value == 0 and self.check_can_leap(x, y, RIGHT):
             moves.append([(x + 2, y), 1])
+
         # Check leap left
-        if x - 2 in range(BOARD_SIZE) and self.board[y][x - 1].value != 0 and self.board[y][x - 2].value == 0:
-            if DEBUG: print("leap left")
+        if x - 2 in range(BOARD_SIZE) and self.board[y][x - 1].value != 0 and self.board[y][x - 2].value == 0 and self.check_can_leap(x, y, LEFT):
             moves.append([(x - 2, y), 1])
-
         return moves
-
+    
     def move_piece(self, xi, yi, xf, yf):
         if (xf, yf) in [move[0] for move in self.get_moves(xi, yi)]:
+            # Update player pieces list
+            if self.board[yi][xi].value == 1:
+                self.p1_pieces = [[xf, yf] if coords == [xi, yi] else coords for coords in self.p1_pieces]
+            elif self.board[yi][xi].value == 2:
+                self.p1_pieces = [[xf, yf] if coords == [xi, yi] else coords for coords in self.p2_pieces]
+            # Move piece
             self.board[yf][xf], self.board[yi][xi] = self.board[yi][xi], self.board[yf][xf]
         else:
-            if DEBUG: print("INVALID MOVE")
+            if DEBUG:
+                print("INVALID MOVE")
+
+    def evaluate(self, player):
+        # Given a specific board state, evaluate it for the player passed as parameter
+        # Evaluation: manhattan distance to oponnent corner
+        evaluation = 0
+        if player == 1:
+            for piece in self.p1_pieces:
+                evaluation -= manhattan_distance(piece, player)
+        if player == 2:
+            for piece in self.p2_pieces:
+                evaluation -= manhattan_distance(piece, player)
+        return evaluation
+
+    def check_walls_simple_move(self, source_x, source_y, direction):
+        cost = 1
+        if direction == UP:
+            if self.board[source_y][source_x].has_wall_up():
+                cost += 1
+                if self.board[source_y-1][source_x].has_wall_down():
+                    cost += 1
+        if direction == DOWN:
+            if self.board[source_y][source_x].has_wall_down():
+                cost += 1
+                if self.board[source_y+1][source_x].has_wall_up():
+                    cost += 1
+        if direction == LEFT:
+            if self.board[source_y][source_x].has_wall_left():
+                cost += 1
+                if self.board[source_y][source_x-1].has_wall_right():
+                    cost += 1
+        if direction == RIGHT:
+            if self.board[source_y][source_x].has_wall_right():
+                cost += 1
+                if self.board[source_y][source_x+1].has_wall_left():
+                    cost += 1
+        return cost
+    
+    def check_can_leap(self, source_x, source_y, direction):
+        if direction == UP:
+            return not (self.board[source_y][source_x].has_wall_up() or self.board[source_y-1][source_x].has_wall_down() or
+                         self.board[source_y-1][source_x].has_wall_up() or self.board[source_y-2][source_x].has_wall_down())
+        if direction == DOWN:
+            return not (self.board[source_y][source_x].has_wall_up() or self.board[source_y+1][source_x].has_wall_down() or
+                         self.board[source_y+1][source_x].has_wall_up() or self.board[source_y-2][source_x].has_wall_up())
+        if direction == LEFT:
+            return not (self.board[source_y][source_x].has_wall_left() and self.board[source_y][source_x-1].has_wall_left() and
+                         self.board[source_y][source_x-1].has_wall_right() and self.board[source_y][source_x-2].has_wall_right())
+        if direction == RIGHT:
+            return not (self.board[source_y][source_x].has_wall_right() and self.board[source_y][source_x+1].has_wall_right() and
+                         self.board[source_y][source_x+1].has_wall_left() and self.board[source_y][source_x+2].has_wall_left())
+
 
 
 
@@ -119,6 +183,5 @@ class GameState:
         for i in range(len(self.board)):
             # print(i, end=" ")
             for j in self.board[i]:
-                print(j, end=" ")
+                print(j.value, end=" ")
             print("")
-
